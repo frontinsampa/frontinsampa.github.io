5f36e9d2cddf3a50786ac83a248c2f9fc36c3967
Keit Oliveira <keitoliveira@outlook.com>
2019-06-23 01:08:42 -0300

    fix(build): Change assets glob for release.

--- .releaserc.js
+++ .releaserc.js
@@ -14,7 +14,7 @@ module.exports = {
         assets: [
           'CHANGELOG.md',
           'README.md',
-          'public/**'
+          './public'
         ],
         releasedLabels: ['pr: released'],
       },
