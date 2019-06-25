4ebf8e40e5e961f6b90f1f290a6a191c50b6a93f
afonsopacifer <afonsopacifer@live.com>
2019-06-25 13:38:46 -0300

    fix: Remove dayjs and add vanilla time formatter

--- src/utils/transformToScheduleHour.js
+++ src/utils/transformToScheduleHour.js
@@ -1,15 +1,31 @@
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
+
+const adjustGMT = (hours) => {
+  const localTime = hours - 3;
+  const withZero = localTime.toString().length === 1 ? `0${localTime}` : localTime;
+  return withZero;
+};
+
+const dateToTime = (date) => {
+  const fullTime = date.split('T')[1];
+  const time = fullTime.substring(0, 5);
+  const hours = time.split(':')[0];
+  const minutes = time.split(':')[1];
+
+  return `${adjustGMT(hours)}:${minutes}`;
+};
 
 /**
  * @param {string} start
  * @param {string} [end]
  * @retuns {string}
  */
+
 const transformToScheduleHour = (start, end = null) => {
-  start = dayjs(start).format('HH:mm');
+  start = dateToTime(start);
 
   if (end) {
-    end = dayjs(end).format('HH:mm');
+    end = dateToTime(end);
   }
 
   return start + (end ? ` - ${end}` : '');
