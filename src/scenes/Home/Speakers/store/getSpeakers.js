93e1a31812176b0e2ddca9f76292ece73fa585d1
Keit Oliveira <keitoliveira@outlook.com>
2020-07-16 12:10:00 -0300

    feat: add query by year

--- src/scenes/Home/Speakers/store/getSpeakers.js
+++ src/scenes/Home/Speakers/store/getSpeakers.js
@@ -1,9 +1,10 @@
-import { getDocument } from '../../../../services/content';
+import { getSpeaker } from '../../../../services/content';
 
 import { SET_SPEAKER } from '.';
 
 export default () => async (dispatch) => {
-  const response = await getDocument('speaker');
+  const year = new Date().getFullYear().toString();
+  const response = await getSpeaker({ year });
 
   const content = response.map(({ data }) => ({ ...data }));
 
--- src/services/content/index.js
+++ src/services/content/index.js
@@ -1,2 +1,3 @@
 export { default as getDocument } from './getDocument';
 export { default as getDocumentByUID } from './getDocumentByUID';
+export { default as getSpeaker } from './getSpeaker';
