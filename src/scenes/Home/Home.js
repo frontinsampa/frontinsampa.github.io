22a7f027afce1409faa1ab67bfce1eb84be3ddcc
Keit Oliveira <keitoliveira@outlook.com>
2020-07-16 12:52:03 -0300

    chore: hide sections

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 import React from 'react';
 
 import Hero from './Hero';
 import About from './About';
 import Speakers from './Speakers';
 import Timeline from './Timeline';
-import Sponsors from './Sponsors';
-import Partners from './Partners';
+// import Sponsors from './Sponsors';
+// import Partners from './Partners';
 import Contact from './Contact';
 
 const Home = () => (
@@ -14,8 +15,6 @@ const Home = () => (
     <About />
     <Speakers />
     <Timeline />
-    <Sponsors />
-    <Partners />
     <Contact />
   </React.Fragment>
 );
