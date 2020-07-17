0af1ab538a283daa003a4249bb9499af71664383
Keit Oliveira <keitoliveira@outlook.com>
2020-07-17 01:04:03 -0300

    feat(commons): improve footer

--- src/commons/Footer/Footer.js
+++ src/commons/Footer/Footer.js
@@ -3,22 +3,27 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
 
 const Footer = () => (
-  <Grid
-    container
-    component="footer"
-    justify="center"
-  >
-    <Grid item xs="auto">
-      <Link
-        component={RouterLink}
-        to="/code-of-conduct"
-      >
-        Código de conduta
-      </Link>
+  <Box mb={5}>
+    <Grid
+      container
+      component="footer"
+      justify="center"
+    >
+      <Grid item xs="auto">
+        <Link
+          component={RouterLink}
+          to="/code-of-conduct"
+          color="textSecondary"
+          variant="body1"
+        >
+          Código de conduta
+        </Link>
+      </Grid>
     </Grid>
-  </Grid>
+  </Box>
 );
 
 export default Footer;
